89cec6e7278208fa4000c36b8681c2e09c04366b
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2026-05-26 08:00:00 +0800

    style: 代码格式化

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,7 @@
-import { expect, test } from "vite-plus/test";
-import { fn } from "../src/index.ts";
+import { expect, test } from 'vite-plus/test'
 
-test("fn", () => {
-  expect(fn()).toBe("Hello, tsdown!");
-});
+import { fn } from '../src/index.ts'
+
+test('fn', () => {
+  expect(fn()).toBe('Hello, tsdown!')
+})
